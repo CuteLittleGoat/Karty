1c832da87ab35374cf2186cdb7d4ed0e22f0d458
CuteLittleGoat <taryf@o2.pl>
2026-02-06 10:25:50 +0100

    Update firebase-config.js

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -3,10 +3,10 @@
 // Firebase Console → Project settings → Your apps (Web) → Firebase SDK snippet (Config)
 
 window.firebaseConfig = {
-  apiKey: "AIzaSyA4m8ZkI-L9MYgKcbAtt8gJaTtr2TlCN5s",
-  authDomain: "audiorpg-2eb6f.firebaseapp.com",
-  projectId: "audiorpg-2eb6f",
-  storageBucket: "audiorpg-2eb6f.firebasestorage.app",
-  messagingSenderId: "848367463263",
-  appId: "1:848367463263:web:500bdf5c394ac786e2e4ea"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
