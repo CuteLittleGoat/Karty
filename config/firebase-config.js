d2088e9d9ddfb24da0ed1e36c19ecca06e6d1347
CuteLittleGoat <taryf@o2.pl>
2026-02-06 15:19:04 +0100

    Update firebase-config.js

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -3,10 +3,10 @@
 // Firebase Console → Project settings → Your apps (Web) → Firebase SDK snippet (Config)
 
 window.firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBjSijsTEvkOF9oTPOf3FgTf3zCcM59rQY",
+  authDomain: "karty-turniej.firebaseapp.com",
+  projectId: "karty-turniej",
+  storageBucket: "karty-turniej.firebasestorage.app",
+  messagingSenderId: "716608782712",
+  appId: "1:716608782712:web:27d29434f013a5cf31888d"
 };
