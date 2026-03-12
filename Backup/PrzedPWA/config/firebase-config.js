bc75e26cd6bd1b5b74b5203b7101d822f49c3609
CuteLittleGoat <taryf@o2.pl>
2026-03-12 04:58:43 +0100

    Add files via upload

---
+++ Backup/PrzedPWA/config/firebase-config.js
@@ -0,0 +1,16 @@
+// Skopiuj ten plik jako: config/firebase-config.js (bez ".template")
+// Następnie wklej swój firebaseConfig z:
+// Firebase Console → Project settings → Your apps (Web) → Firebase SDK snippet (Config)
+
+window.firebaseConfig = {
+  apiKey: "AIzaSyBjSijsTEvkOF9oTPOf3FgTf3zCcM59rQY",
+  authDomain: "karty-turniej.firebaseapp.com",
+  projectId: "karty-turniej",
+  storageBucket: "karty-turniej.firebasestorage.app",
+  messagingSenderId: "716608782712",
+  appId: "1:716608782712:web:27d29434f013a5cf31888d",
+  tablesCollection: "Tables",
+  gamesCollection: "Tables",
+  gameDetailsCollection: "rows",
+  userGamesCollection: "UserGames"
+};
